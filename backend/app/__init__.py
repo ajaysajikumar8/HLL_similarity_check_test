11881b532b0efab7e47abb6ad37b15fb55f5021d
Ajay Sajikumar <ajaysajikumar8@gmail.com>
2024-08-05 16:21:14 +0530

    added .env example

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from .utils import setup_logging
 from .db import db
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = (
-        "postgresql://postgres:password@localhost/postgres"
-    )
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
